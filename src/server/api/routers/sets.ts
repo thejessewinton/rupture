338377a15b6b93de15ac9a9ee048530d3a53076e
Jesse Winton <jrandallwinton@gmail.com>
2024-03-20 22:02:08 -0400

    update

--- src/server/api/routers/sets.ts
+++ src/server/api/routers/sets.ts
@@ -26,7 +26,7 @@ export const setsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       await ctx.db.transaction(async (db) => {
-        const [latestComposition] = await db.query.composition.findMany({
+        const [latestComposition] = await db.query.compositions.findMany({
           where: eq(compositions.user_id, ctx.session.user.id),
           orderBy: [desc(compositions.created_at)]
         })
