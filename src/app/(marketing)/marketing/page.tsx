a0e922afcab8f2365d707f53e2df9918f6449f8a
Jesse Winton <jrandallwinton@gmail.com>
2024-03-23 20:20:29 -0400

    meh

---
+++ src/app/(marketing)/marketing/page.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div className='container mx-auto flex items-center px-4'>
+      <h1 className='bg-gradient-to-br from-white to-neutral-700 bg-clip-text text-5xl leading-[1] text-transparent md:text-[6rem]'>
+        Workout stats for
+        <br />
+        serious lifters
+      </h1>
+    </div>
+  )
+}
